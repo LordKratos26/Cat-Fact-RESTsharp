0e08373d9dbe59fe14812af7440e49e65cdc1b63
Elxan <34681883+LordKratos26@users.noreply.github.com>
2019-04-05 19:35:33 +0400

    Add files via upload

---
+++ Facts/Facts/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+namespace Facts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> items = new List<string>();
+            items.Add("cat");
+            items.Add("dog");
+            items.Add("horse");
+
+
+
+            comboBox1.Items.AddRange(items.ToArray());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string type = comboBox1.Text;
+            decimal amount = numericUpDown1.Value;
+            var client = new RestClient($"https://cat-fact.herokuapp.com/facts/random?animal_type={type}&amount={amount}");
+            var request = new RestRequest(Method.GET);
+            List<response> response = client.Execute<List<response>>(request).Data;
+            dataGridView1.DataSource = response;
+        }
+    }
+}
---
+++ Facts/Facts/response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facts
+{
+    public class response
+    {
+        public string _id { get; set; }
+        public DateTime updatedAt { get; set; }
+        public DateTime createdAt { get; set; }
+        public string user { get; set; }
+        public string text { get; set; }
+        public int __v { get; set; }
+        public bool deleted { get; set; }
+        public string type { get; set; }
+        public string source { get; set; }
+        public bool used { get; set; }
+    }
+}
